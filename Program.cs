57eeb0b23d7802bef17f0b13605297cb419a30f9
Taha Kaplan <taha0kaplan@gmail.com>
2024-11-07 13:50:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEachDöngüsü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayılar = { 15, 55, 85, 14, 65, 87 };
+            foreach (var sayi in sayılar) 
+            {
+                Console.WriteLine(sayi);
+            }
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine(sayılar[0] + " " + sayılar[1] + " " + sayılar[5]);
+            Console.ReadLine();
+        }
+    }
+}
